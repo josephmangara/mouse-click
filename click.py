6d5fcde801df3a00d503827a247aabba76f03a80
josephmangara <josephmangara2@gmail.com>
2025-09-01 12:44:17 +0300

    showing clicks on white canvas

--- click.py
+++ click.py
@@ -3,25 +3,30 @@ from pynput import mouse
 import threading
 import time
 
-
-# Tkinter window setup 
 root = tk.Tk()
-root.attributes("-topmost", True)
-root.overrideredirect(True)
-root.attributes("-transparentcolor", "black")
-root.geometry(f"{root.winfo_screenmmwidth()}x{root.winfo_screenmmheight()}+0+0")
-root.configure(bg="black")
+root.attributes("-topmost", True)   
+root.overrideredirect(True)        
+root.attributes("-fullscreen", True)  
+root.configure(bg="white")          
 
-canvas = tk.Canvas(root, bg="black", highlightthickness=0)
+canvas = tk.Canvas(root, bg="white", highlightthickness=0)
 canvas.pack(fill="both", expand=True)
 
-def show_circle(x,y):
-    r = 20
-    circle = canvas.create_oval(x-r, y-r, x+r, y+r, outline="blue", width=3)
+def show_circle(x, y):
+    r = 20  
+    circle = canvas.create_oval(x-r, y-r, x+r, y+r, outline="red", width=3)
 
     def remove_circle():
-        time.sleep(1)
+        time.sleep(1)  
         canvas.delete(circle)
-    
+
     threading.Thread(target=remove_circle, daemon=True).start()
 
+def on_click(x, y, button, pressed):
+    if pressed:
+        root.after(0, show_circle, x, y)
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+
+root.mainloop()
