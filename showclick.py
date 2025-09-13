ac6ec4994ade86cea92cd1bbdb1948c7a057d909
josephmangara <josephmangara2@gmail.com>
2025-09-13 23:54:53 +0300

    created circle

--- showclick.py
+++ showclick.py
@@ -4,6 +4,21 @@ from tkinter import ttk
 
 mouse = Controller()
 root = Tk()
-ttk.Button(root, text="").grid()
+root.title("circle")
+
+canvas = Canvas(root, width=100, height=100, bg="white")
+canvas.pack()
+
+center_x = 50
+center_y = 50
+radius = 25
+
+x0 = center_x - radius
+y0 = center_y - radius
+x1 = center_x + radius
+y1 = center_y + radius
+
+canvas.create_oval(x0, y0, x1, y1, fill="red", outline="darkred", width=2)
+
 
 root.mainloop()
