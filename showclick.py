c19c591a650ac7c2bd01dfa3bdb85f7b055bb846
josephmangara <josephmangara2@gmail.com>
2025-09-13 23:50:59 +0300

    tkinter grid

--- showclick.py
+++ showclick.py
@@ -1,22 +1,9 @@
 from pynput.mouse import Button, Controller
+from tkinter import *
+from tkinter import ttk
 
 mouse = Controller()
+root = Tk()
+ttk.Button(root, text="").grid()
 
-# read pointer position 
-print('the current pointer position is {}'.format(mouse.position))
-
-mouse.position = (10, 20)
-print('Now we have moved it to {0}'.format(
-    mouse.position))
-
-mouse.press(Button.left)
-mouse.release(Button.left)
-
-# monitoring mouse position 
-def on_click(x,y, button, pressed, injected):
-    print('{} at {}; it was {}'.format(
-        'pressed' if pressed else 'released',
-        (x,y, 'faked' if injected else "not faked")
-    ))
-    if not pressed:
-        return False
+root.mainloop()
